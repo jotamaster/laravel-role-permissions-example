1530d5ff232d7aa91763ba7392ba9a3f99f653fe
jotamaster <j3anhernandez@gmail.com>
2020-08-10 22:27:02 -0400

    order tab

--- database/seeds/CreateAdminUserSeeder.php
+++ database/seeds/CreateAdminUserSeeder.php
@@ -10,16 +10,16 @@ class CreateAdminUserSeeder extends Seeder
 *
 * @return void
 */
-public function run()
-{
-$user = User::create([
-'name' => 'Rahul Shukla',
-'email' => 'admin@rscoder.com',
-'password' => bcrypt('123456')
-]);
-$role = Role::create(['name' => 'Admin']);
-$permissions = Permission::pluck('id','id')->all();
-$role->syncPermissions($permissions);
-$user->assignRole([$role->id]);
-}
+    public function run()
+    {
+        $user = User::create([
+        'name' => 'Rahul Shukla',
+        'email' => 'admin@rscoder.com',
+        'password' => bcrypt('123456')
+        ]);
+        $role = Role::create(['name' => 'Admin']);
+        $permissions = Permission::pluck('id','id')->all();
+        $role->syncPermissions($permissions);
+        $user->assignRole([$role->id]);
+    }
 }
